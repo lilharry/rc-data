bd74f38e6f5d8027473d69738b8178c075cd7120
Harry Lum <hlum1@stuy.edu>
2017-04-05 23:56:32 -0400

    add axis labels

--- static/line.js
+++ static/line.js
@@ -1,18 +1,21 @@
 // Set the dimensions of the canvas / graph
-var margin = {top: 30, right: 20, bottom: 80, left: 50},
+var margin = {top: 30, right: 20, bottom: 80, left: 100},
     width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // Parse the date / time
 var parseDate = d3.time.format("%m/%Y").parse;	
 
+var minDate = new Date(2015,8,1)
+var maxDate = new Date(2016,9,1)
+
 // Set the ranges
-var x = d3.time.scale().range([0, width]);
+var x = d3.time.scale().domain([minDate,maxDate]).range([0, width]);
 var y = d3.scale.linear().range([height, 0]);
 
 // Define the axes
 var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(14);
+    .orient("bottom");
 
 var yAxis = d3.svg.axis().scale(y)
     .orient("left").ticks(10);
@@ -31,6 +34,7 @@ var svg = d3.select("body")
     .append("g")
         .attr("transform", 
               "translate(" + margin.left + "," + margin.top + ")");
+    
 
 d3.csv("/static/events.csv", function(error, data) {
     data.forEach(function(d) {
@@ -56,9 +60,21 @@ d3.csv("/static/events.csv", function(error, data) {
                  .attr("dx", "-.8em")
                  .attr("dy", ".15em")
                  .attr("transform", "rotate(-65)");
-                // Add the Y Axis
+    svg.append("text")
+        .attr("x", width / 2 )
+        .attr("y", height + margin.bottom)
+        .style("text-anchor", "middle")
+        .text("Date");
+
+    // Add the Y Axis
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
-
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Hours");
 });
